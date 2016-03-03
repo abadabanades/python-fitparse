01aabbe08ebc5ea384587084984221a3dd909721
Daniel Abad <abadabanades@gmail.com>
2016-03-03 11:28:00 +0100

    added UTC_REFERENCE as a unique definition of the initial datetime

--- fitparse/processors.py
+++ fitparse/processors.py
@@ -1,4 +1,7 @@
-import datetime
+from datetime import datetime
+
+# Datetimes (uint32) represent seconds since this UTC_REFERENCE
+UTC_REFERENCE = 631065600  # timestamp for UTC 00:00 Dec 31 1989
 
 
 class FitFileDataProcessor(object):
@@ -14,12 +17,12 @@ class FitFileDataProcessor(object):
     def process_type_date_time(self, field_data):
         value = field_data.value
         if value is not None and value >= 0x10000000:
-            field_data.value = datetime.datetime.utcfromtimestamp(631065600 + value)
+            field_data.value = datetime.utcfromtimestamp(UTC_REFERENCE + value)
             field_data.units = None  # Units were 's', set to None
 
     def process_type_local_date_time(self, field_data):
         if field_data.value is not None:
-            field_data.value = datetime.datetime.fromtimestamp(631065600 + field_data.value)
+            field_data.value = datetime.fromtimestamp(UTC_REFERENCE + field_data.value)
             field_data.units = None
 
 
